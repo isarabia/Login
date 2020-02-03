67996e8f9acbc0c9a2db017e9d63a6bfd1776859
isarabia19 <isarabia19@ESCUELA.local>
2020-02-03 14:49:40 +0100

    cefem pay listo, falta incluir los campos de cada vista

--- js/swap.js
+++ js/swap.js
@@ -1,14 +1,27 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
+var x = document.getElementById("datos");
+var y = document.getElementById("compra");
 var z = document.getElementById("btn");
+var v = document.getElementById("finalizar");
+var w = document.getElementById("social");
 
-function register() {
-    x.style.left = "-400px";
-    y.style.left = "50px";
-    z.style.left = "110px";
-}
-function login() {
+function datos() {
     x.style.left = "50px";
     y.style.left = "450px";
     z.style.left = "0";
+    v.style.left = "850px";
+    w.style.marginTop = "300px";
+}
+function compra() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "90px";
+    v.style.left = "450px";
+    w.style.marginTop = "600px";
+}
+function finalizar() {
+    x.style.left = "-800px";
+    y.style.left = "-450px";
+    z.style.left = "200px";
+    v.style.left = "50px";
+    w.style.marginTop = "300px";
 }
